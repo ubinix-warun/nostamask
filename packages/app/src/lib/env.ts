4c59995e5598f5504f94ed7aa06a24586968520a
ubinix-warun <warun@ubinix.com>
2023-08-21 21:50:56 +0700

    ADD: ENV=NEXT_PUBLIC_SNAP_ORIGIN

--- packages/app/src/lib/env.ts
+++ packages/app/src/lib/env.ts
@@ -8,4 +8,4 @@ export const siteURL = process.env.NEXT_PUBLIC_URL ?? `http://localhost:3000` as
  * Will default to the local hosted snap if no value is provided in environment.
  */
 export const defaultSnapOrigin =
-  process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
+  process.env.NEXT_PUBLIC_SNAP_ORIGIN ?? `local:http://localhost:8080`;
